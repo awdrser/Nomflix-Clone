bde0d4f0c00b320df96f33fb93bfe5a628e97723
AWDRSER <ddan990@naver.com>
2025-09-06 23:03:19 +0900

    9-5

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import App from "./App.tsx";
 import { theme } from "./theme.ts";
@@ -70,11 +71,15 @@ a {
 }
 `;
 
+const client = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
